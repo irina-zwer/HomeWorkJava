26646e2ded2c607417d9ea7e478cfca366d48e03
irina-zwer <73507721+irina-zwer@users.noreply.github.com>
2021-01-19 18:52:15 +0300

    Домашнее задание 4. Часть 1.

---
+++ src/main/java/Java3Homework4/ABC.java
@@ -0,0 +1,57 @@
+package Java3Homework4;
+
+public class ABC {
+    static Object mon = new Object();
+    static volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (mon) {
+                        while (currentLetter != 'A') {
+                            mon.wait();
+                        }
+                        System.out.println("A");
+                        currentLetter = 'B';
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (mon) {
+                        while (currentLetter != 'B') {
+                            mon.wait();
+                        }
+                        System.out.println("B");
+                        currentLetter = 'C';
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (mon) {
+                        while (currentLetter != 'C') {
+                            mon.wait();
+                        }
+                        System.out.println("C");
+                        currentLetter = 'A';
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
