a1b8864643489a28b8a27959c58318f831ffd743
irina-zwer <73507721+irina-zwer@users.noreply.github.com>
2021-01-22 19:13:33 +0300

    Домашнее задание 5. Гонки.

---
+++ src/main/java/Java3Homework5/Race.java
@@ -0,0 +1,9 @@
+package Java3Homework5;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
